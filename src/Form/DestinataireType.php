eecabb71827e43e07541b08807ad6764215802b8
Cyliana <a.taddei@codeur.online>
2021-11-10 08:40:42 +0100

    add attribut for destinataire

--- src/Form/DestinataireType.php
+++ src/Form/DestinataireType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Destinataire;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,16 +14,68 @@ class DestinataireType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('fonction')
-            ->add('denomination')
-            ->add('adresse')
-            ->add('codePostal')
-            ->add('localite')
-            ->add('telephone')
-            ->add('mail')
-            ->add('commentaire')
+            ->add('nom', TextType::class, [
+                'label'=>'Nom',
+                'attr'=>['placeholder'=>'Votre nom',
+                    'class'=>'form-control'],
+                'label_attr'=>['class'=>'form-label'],
+            ])
+            ->add('prenom', TextType::class, [
+                'label'=>'Prénom',
+                'attr'=>['placeholder'=>'Votre prénom',
+                    'class'=>'form-control'],
+                'label_attr'=>['class'=>'form-label'],
+            ])
+            ->add('fonction',TextType::class, [
+                'label'=>'Fonction',
+                'attr'=>['placeholder'=>'Fonction au sein de l\'entreprise',
+                    'class'=>'form-control'],
+                'label_attr'=>['class'=>'form-label'],
+            ])
+
+            ->add('denomination',TextType::class, [
+                'label'=>'Dénomination',
+                'attr'=>['placeholder'=>'Nom de l\'entreprise',
+                    'class'=>'form-control'],
+                'label_attr'=>['class'=>'form-label'],
+            ])
+
+            ->add('adresse', TextType::class, [
+                'label'=>'Votre adresse',
+                'attr'=>['placeholder'=>'N° de voie,rue, impasse, etc.',
+                    'class'=>'form-control'],
+                'label_attr'=>['class'=>'form-label'],
+            ])
+            ->add('codePostal', TextType::class, [
+                'label'=>'Code postal',
+                'attr'=>['placeholder'=>'Votre code postal',
+                    'class'=>'form-control'],
+                'label_attr'=>['class'=>'form-label'],
+            ])
+            ->add('localite', TextType::class, [
+                'label'=>'Ville',
+                'attr'=>['placeholder'=>'votre ville',
+                    'class'=>'form-control'],
+                'label_attr'=>['class'=>'form-label'],
+            ])
+            ->add('telephone', TextType::class, [
+                'label'=>'N° de téléphone',
+                'attr'=>['placeholder'=>'votre n° de téléphone',
+                    'class'=>'form-control'],
+                'label_attr'=>['class'=>'form-label'],
+            ])
+            ->add('mail', EmailType::class, [
+                'label'=>'Email',
+                'attr'=>['placeholder'=>'votre adresse e-mail',
+                    'class'=>'form-control'],
+                'label_attr'=>['class'=>'form-label'],
+            ])
+            ->add('commentaire', TextareaType::class,[
+                'label'=>'Commentaire',
+                'attr'=>['placeholder'=>'Ecrivez un commentaire',
+                    'class'=>'form-control'],
+                'label_attr'=>['class'=>'form-label'],
+            ])
             ->add('utilisateur')
         ;
     }
